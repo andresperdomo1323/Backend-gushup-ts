990458da4a302725a6b32970785ce5323fe3eec6
JeannethMedrano14 <141864876+JeannethMedrano14@users.noreply.github.com>
2023-11-17 09:40:33 -0500

    Envío campañas

---
+++ src/routes/campaignRoutes.ts
@@ -0,0 +1,57 @@
+import express, { Router, Request, Response } from 'express';
+import axios, { AxiosError } from 'axios';
+import { getContactsFromDatabase } from '../models/contactModel';
+import { TextMessage } from '../types';
+
+const router: Router = express.Router();
+
+router.post('/enviar-campana', async (req: Request, res: Response) => {
+  try {
+    const textMessage: TextMessage = {
+      type: 'text',
+      text: '¡Hola! Este es un mensaje de la campaña.',
+    };
+
+    const phoneNumbers: string[] = await getContactsFromDatabase();
+
+    for (const phoneNumber of phoneNumbers) {
+      const messageData = {
+        ...textMessage,
+        type: 'text',
+        text: `¡Hola! Este es un mensaje de la campaña para ${phoneNumber}.`,
+        phoneNumber,
+      };
+
+      const encodedParamsForCampaign = new URLSearchParams();
+      encodedParamsForCampaign.set('message', JSON.stringify(messageData));
+      encodedParamsForCampaign.set('channel', 'whatsapp');
+      encodedParamsForCampaign.set('source', '573154288560');
+      encodedParamsForCampaign.set('destination', phoneNumber);
+
+      const optionsForCampaign = {
+        method: 'POST',
+        url: 'https://api.gupshup.io/sm/api/v1/msg',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'apikey': process.env.API_KEY,
+        },
+        data: encodedParamsForCampaign,
+      };
+
+      try {
+        const response = await axios(optionsForCampaign);
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error al enviar el mensaje:', (error as AxiosError).response ? (error as AxiosError).response?.data : (error as any).message);
+        console.log('Detalles del error:', error);
+      }
+    }
+
+    res.send('Mensajes de la campaña enviados con éxito');
+  } catch (error) {
+    console.error('Error al procesar la campaña:', error);
+    res.status(500).send('Error al procesar la campaña');
+  }
+});
+
+export default router;
